5290858818915038aa9910b8bd07bb5aec730166
Abubakar Riaz <abubakar.riaz@outlook.com>
2019-06-03 15:01:53 +0500

    product and category class added naming convention applied on namespace and assembly

---
+++ OnlineShopASPNETFrameworkWebAPI/ComponentLibrary/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.CompanyName.OnlineShop.ComponentLibrary
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public int MyProperty { get; set; }
+        public string CategoryName { get; set; }
+        public string SubCategoryName { get; set; }
+
+        public ICollection<Product> Products  { get; set; }
+    }
+}
--- OnlineShopASPNETFrameworkWebAPI/ComponentLibrary/Properties/AssemblyInfo.cs
+++ OnlineShopASPNETFrameworkWebAPI/ComponentLibrary/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ComponentLibrary")]
+[assembly: AssemblyTitle("Com.CompanyName.OnlineShop.ComponentLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ComponentLibrary")]
+[assembly: AssemblyProduct("Com.CompanyName.OnlineShop.ComponentLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
